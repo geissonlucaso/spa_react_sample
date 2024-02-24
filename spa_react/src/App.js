5dc2f02a886943bfab70fda263a02a833cf0f4af
Geisson Oliveira <geissonlucaso@gmail.com>
2024-02-23 21:57:16 -0300

    Definição das rotas.

--- spa_react/src/App.js
+++ spa_react/src/App.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
+import { Route, NavLink, HashRouter, Routes } from "react-router-dom";
+
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import NotFound from './NotFound';
 
 class App extends Component {
     render() {
         return (
-            <div className='App'>
-                <h1>A Simple SPA made using React</h1>
-                <ul className='header'>
-                    <li><a href='/'>Home</a></li>
-                    <li><a href='/about'>About</a></li>
-                    <li><a href='/contact'>Contact</a></li>
-                </ul>
-                <div className='pageContent'>
-
+            <HashRouter>
+                <div className='App'>
+                    <h1>A Simple SPA made using React</h1>
+                    <ul className='header'>
+                        <li><NavLink to='/'>Home</NavLink></li>
+                        <li><NavLink to='/about'>About</NavLink></li>
+                        <li><NavLink to='/contact'>Contact</NavLink></li>
+                    </ul>
+                    <div className='content'>
+                        <Routes>
+                            <Route exact path='/' Component={Home} />
+                            <Route path='/about' Component={About} />
+                            <Route path='/contact' Component={Contact} />
+                            <Route path='*' Component={NotFound} />
+                        </Routes>
+                    </div>
                 </div>
-            </div>
+            </HashRouter>
         );
     }
 }
